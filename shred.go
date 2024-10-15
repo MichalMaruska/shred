44c5ccd1a69d7e76f6111a45b1fa899d7d384a81
Michal Maruska <mmaruska@gmail.com>
2024-10-15 08:19:03 +0200

    format: use spaces, clean comments

--- shred.go
+++ shred.go
@@ -5,79 +5,78 @@
 package shred
 
 import (
-	"bufio"
-	"crypto/rand"
-	"errors"
-	"io/fs"
-	"os"
+        "bufio"
+        "crypto/rand"
+        "errors"
+        "io/fs"
+        "os"
 )
 
-// public for testing:
+// public only for testing:
 func Overwrite(path string) error {
-	var BLOCKSIZE int64 = 4096
-	// variable declaration
-	var err error
-	var lstat fs.FileInfo
-	var fd *os.File
-	var sizeleft int64
-	var wr *bufio.Writer
+        var BLOCKSIZE int64 = 4096
+        var err error
+        var lstat fs.FileInfo
+        var fd *os.File
+        var sizeleft int64
+        var wr *bufio.Writer
 
-	// 1st Lstat() to verify it is a regular file
-	lstat, err = os.Lstat(path)
-	if err != nil {
-		return err
-	}
-	if !lstat.Mode().IsRegular() {
-		return errors.New("only regular files can be shredded")
-	}
+        // 1st Lstat() to verify it is a regular file
+        lstat, err = os.Lstat(path)
+        if err != nil {
+                return err
+        }
+        if !lstat.Mode().IsRegular() {
+                return errors.New("only regular files can be shredded")
+        }
 
-	// 2nd let's open the file
-	fd, err = os.OpenFile(path, os.O_RDWR, 0)
-	if err != nil {
-		return err
-	}
-	defer fd.Close()
+        // 2nd let's open the file
+        fd, err = os.OpenFile(path, os.O_RDWR, 0)
+        if err != nil {
+                return err
+        }
+        defer fd.Close()
 
-	// use buffered io for performance
-	wr = bufio.NewWriter(fd)
+        // use buffered io for performance
+        wr = bufio.NewWriter(fd)
+        buffer := make([]byte, BLOCKSIZE)
 
-	buffer := make([]byte, BLOCKSIZE)
+        // write random data in blocks
+        // lseek(0)
+        for sizeleft = lstat.Size(); sizeleft > 0; sizeleft -= BLOCKSIZE {
+                // avoid generating useless random data, so shorten:
+                if sizeleft < 4096 {
+                        buffer = make([]byte, sizeleft)
+                }
+                _, err = rand.Read(buffer)
+                if err != nil {
+                        return err
+                }
 
-	// write random data in blocks
-	// lseek(0)
-	for sizeleft = lstat.Size(); sizeleft > 0; sizeleft -= BLOCKSIZE {
-		// avoid generating useless random data, so shorten:
-		if sizeleft < 4096 {
-			buffer = make([]byte, sizeleft)
-		}
-		_, err = rand.Read(buffer)
-		if err != nil {
-			return err
-		}
+                // error handling?
+                _, err = wr.Write(buffer)
+                if err != nil {
+                        return err
+                }
+        }
 
-		// error handling?
-		_, err = wr.Write(buffer)
-		if err != nil {
-			return err
-		}
-	}
-
-	wr.Flush()
-	fd.Sync()
-	return nil
+        // no error handling
+        wr.Flush()
+        fd.Sync()
+        return nil
 }
 
 func Shred(path string) error {
-	var err error
-	for i := 0; i < 3; i++ {
-		err = Overwrite(path)
-		if err != nil {
-			return err
-		}
-	}
-	err = os.Remove(path)
-	if err != nil {
-		return err
-	}
-	return nil
+        var err error
+        for i := 0; i < 3; i++ {
+                err = Overwrite(path)
+                if err != nil {
+                        return err
+                }
+        }
+        err = os.Remove(path)
+        if err != nil {
+                return err
+        }
+        return nil
 }
